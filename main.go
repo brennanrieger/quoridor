8b95c2f27266bfa1bd4f3138afa3306711fbdd16
Brennan Rieger <brieger@liveramp.com>
2018-08-29 12:00:57 -0400

    progress on featurePlayer

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ import (
 // TODO: make Matrix non-exported (matrix) - actually this might not work...
 // TODO: curPlayer is probably a board concern, not game concern
 // TODO: remove stutter wiht player package
-// TODO: make random player rely on availableMoves and flush out availableMoves
 // TODO: move as an action and a struct is confusing
 
 func main() {
 	av := &visualizer.AsciiVisualizer{}
 	game := &game.Game{}
 	p0 := &player.HumanPlayer{}
-	p1 := &player.HumanPlayer{}
+	p1 := &player.FeaturePlayer{}
+	p1.Init(true)
 	game.Init(9, 11, p0, p1, av)
 	bool := game.Play()
 	if bool {
