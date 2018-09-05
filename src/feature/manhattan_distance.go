11a9a614914757fb664279e218c974ea58412a4c
Brennan Rieger <brieger@liveramp.com>
2018-09-05 11:44:45 -0400

    no longer segfaulting

--- src/feature/manhattan_distance.go
+++ src/feature/manhattan_distance.go
@@ -4,6 +4,7 @@ package feature
 
 import (
 	"board"
+	"fmt"
 	"util"
 )
 
@@ -16,8 +17,10 @@ type ManhattanDistance struct {
 
 func (md *ManhattanDistance) Val(b *board.Board) int {
 	md.board = b
+	md.distances = &util.ValMatrix{}
 	md.distances.Init(b.NRows, b.NCols)
 	md.neighbors = []*board.Pos{b.Pos0}
+	md.distances.Set(b.Pos0, 0)
 
 	for head := 0; md.val == 0; head++ {
 		md.bfs(head)
@@ -27,28 +30,29 @@ func (md *ManhattanDistance) Val(b *board.Board) int {
 }
 
 func (md *ManhattanDistance) bfs(head int) {
+	fmt.Println(head)
 	pos := md.neighbors[head]
 	dist := md.distances.Get(pos).(int)
 	b := md.board
 	curWalker := false //TODO: put this in implementation
 
-	if !b.VertiWalls.Get(pos) && pos.Col != 0 && md.distances.Get(pos.L()) == 0 {
+	if !b.VertiWalls.Get(pos) && pos.Col != 0 && md.distances.Get(pos.L()) == nil {
 		md.distances.Set(pos.L(), dist+1)
 		md.neighbors = append(md.neighbors, pos.L())
 	}
-	if !b.VertiWalls.Get(pos.R()) && pos.Col != b.NCols-1 && md.distances.Get(pos.R()) == 0 {
+	if !b.VertiWalls.Get(pos.R()) && pos.Col != b.NCols-1 && md.distances.Get(pos.R()) == nil {
 		md.distances.Set(pos.R(), dist+1)
 		md.neighbors = append(md.neighbors, pos.R())
 	}
 	if !b.HorizWalls.Get(pos) && pos.Row == 0 && curWalker {
 		md.val = dist + 1
-	} else if !b.HorizWalls.Get(pos) && pos.Row != 0 && md.distances.Get(pos.D()) == 0 {
+	} else if !b.HorizWalls.Get(pos) && pos.Row != 0 && md.distances.Get(pos.D()) == nil {
 		md.distances.Set(pos.D(), dist+1)
 		md.neighbors = append(md.neighbors, pos.D())
 	}
 	if !b.HorizWalls.Get(pos.U()) && pos.Row == b.NRows-1 && !curWalker {
 		md.val = dist + 1
-	} else if !b.HorizWalls.Get(pos.U()) && pos.Row != b.NRows-1 && md.distances.Get(pos.U()) == 0 {
+	} else if !b.HorizWalls.Get(pos.U()) && pos.Row != b.NRows-1 && md.distances.Get(pos.U()) == nil {
 		md.distances.Set(pos.U(), dist+1)
 		md.neighbors = append(md.neighbors, pos.U())
 	}
