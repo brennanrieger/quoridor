94d3d9ee774795e23c2dc892a9e8d97dcd81720b
Brennan Rieger <brennanrieger@gmail.com>
2019-01-22 17:46:11 -0500

    put matrix in matrix_test into suite

--- src/board/matrix_test.go
+++ src/board/matrix_test.go
@@ -6,23 +6,27 @@ import (
 	gc "github.com/go-check/check"
 )
 
-type MatrixSuite struct{}
+type MatrixSuite struct {
+	matrix *Matrix
+}
+
+func (s *MatrixSuite) SetUpSuite(c *gc.C) {
+	s.matrix = &Matrix{
+		NRows: 2,
+		NCols: 3,
+		grid:  []bool{true, true, false, false, true, false},
+	}
+}
 
 func (s *MatrixSuite) TestCopy(c *gc.C) {
-	matrix := &Matrix{
-		NRows: 2,
-		NCols: 2,
-		grid:  make([]bool, 4, 4),
-	}
-
-	// Check matrix is copied by value but not reference
-	c.Check(matrix, gc.Not(gc.Equals), matrix.Copy())
-	c.Check(matrix.NRows, gc.Equals, matrix.Copy().NRows)
-	c.Check(matrix.NCols, gc.Equals, matrix.Copy().NCols)
+	// Check s.matrix is copied by value but not reference
+	c.Check(s.matrix, gc.Not(gc.Equals), s.matrix.Copy())
+	c.Check(s.matrix.NRows, gc.Equals, s.matrix.Copy().NRows)
+	c.Check(s.matrix.NCols, gc.Equals, s.matrix.Copy().NCols)
 
 	// Check grid is copied by value but not reference
-	c.Check(matrix.grid, gc.Not(gc.Equals), matrix.Copy().grid)
-	c.Check(reflect.DeepEqual(matrix.grid, matrix.Copy().grid), gc.Equals, true)
+	c.Check(s.matrix.grid, gc.Not(gc.Equals), s.matrix.Copy().grid)
+	c.Check(reflect.DeepEqual(s.matrix.grid, s.matrix.Copy().grid), gc.Equals, true)
 }
 
 var _ = gc.Suite(new(MatrixSuite))
