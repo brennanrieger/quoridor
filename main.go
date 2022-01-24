b92cbdff54c7d2443790ea3c3f9d7ac157b7824c
brennanrieger <brennanrieger@gmail.com>
2022-01-24 09:57:59 -0500

    fix typo

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 // TODO: read https://pdfs.semanticscholar.org/acad/6962a9bb3eb3fde4272f476d6625eb0a8182.pdf
 // TODO: idea use ML on small scale board to find good heuristics
 // TODO: make Matrix non-exported (matrix) - actually this might not work...
-// TODO: remove stutter wiht player package
+// TODO: remove stutter with player package
 // TODO: add testing
 // TODO: make vertiwall and horizwall functions rather than using get function
 // TODO: remove unnecessary parentheses in if statements
